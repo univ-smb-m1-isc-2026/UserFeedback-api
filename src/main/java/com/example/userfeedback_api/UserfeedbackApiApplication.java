16f02ca1874426e425cfef3f8a5c7535a6012318
clinb <benjaminclin@hotmail.fr>
2026-03-13 16:57:32 +0100

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/example/userfeedback_api/UserfeedbackApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.userfeedback_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserfeedbackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserfeedbackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userfeedback_api/UserfeedbackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userfeedback_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserfeedbackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
